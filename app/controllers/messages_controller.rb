59ca80352d715753fdf8d13ecc9a4bc4c4da25f2
Jennifer Yee <jennifer@jennifer-macbookpro.local>
2017-12-26 15:19:11 -0800

    added method to player translation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,9 +18,9 @@ class MessagesController < ApplicationController
     user = check_user_type params[:auth_token]
     if user.is_a? Coach
       begin
-        japanese = RestClient.post("https://api.microsofttranslator.com/V2/Http.svc/TranslateArray", generate_xml('en', 'ja', params[:message_text]), headers={'Ocp-Apim-Subscription-Key': Rails.application.secrets['jsa_key'], 'Content-Type': 'application/xml'})
-        if japanese.code == 200
-          parsed_response = Nokogiri::HTML(japanese.body)
+        res = RestClient.post("https://api.microsofttranslator.com/V2/Http.svc/TranslateArray", generate_xml('en', 'ja', params[:message_text]), headers={'Ocp-Apim-Subscription-Key': Rails.application.secrets['jsa_key'], 'Content-Type': 'application/xml'})
+        if res.code == 200
+          parsed_response = Nokogiri::HTML(res.body)
           translation = parsed_response.children[1].children.first.children.first.children.first.children[2].children.text
         else
           translation = 'Failed Translation'
@@ -28,9 +28,21 @@ class MessagesController < ApplicationController
       rescue RestClient::ExceptionWithResponse => e
         puts e.response
       end
-      message = Message.new(coach_id: user.id, player_id: params[:player_id], text: params[:message_text], japanese_text: translation, sender: 'coach')
+      message = Message.new(coach_id: user.id, player_id: params[:player_id], japanese_text: params[:message_text], text: translation, sender: 'coach')
     else
-      message = Message.new(coach_id: params[:coach_id], player_id: user.id, text: params[:message_text], sender: 'player')
+      # assuming english to japanese
+      begin
+        res = RestClient.post("https://api.microsofttranslator.com/V2/Http.svc/TranslateArray", generate_xml('ja', 'en', params[:message_text]), headers={'Ocp-Apim-Subscription-Key': Rails.application.secrets['jsa_key'], 'Content-Type': 'application/xml'})
+        if res.code == 200
+          parsed_response = Nokogiri::HTML(res.body)
+          translation = parsed_response.children[1].children.first.children.first.children.first.children[2].children.text
+        else
+          translation = 'Failed Translation'
+        end
+      rescue RestClient::ExceptionWithResponse => e
+        puts e.response
+      end
+      message = Message.new(coach_id: params[:coach_id], player_id: user.id, text: params[:message_text], japanese_text: translation, sender: 'player')
     end
     if message.save
       render json: {success: 'Success!'}
