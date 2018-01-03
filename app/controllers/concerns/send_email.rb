ad8b08dca61fb2d5f970bf9c84bdc29a59a42846
Jennifer Yee <jennifer@jennifer-macbookpro.local>
2018-01-02 20:25:44 -0800

    fixed email to say player name or coach email

--- app/controllers/concerns/send_email.rb
+++ app/controllers/concerns/send_email.rb
@@ -5,9 +5,10 @@ module SendEmail
   extend ActiveSupport::Concern
   include SendGrid
   def send_email(recipient, sender, text)
+    name = sender.class.to_s == 'Coach' ? sender.id : sender.name
     from = Email.new(email: 'no-reply@nippon-softball.com')
     to = Email.new(email: recipient.email)
-    subject = "You've received a message from #{sender.class.to_s} #{sender.name || sender.id}"
+    subject = "You've received a message from #{sender.class.to_s} #{name}"
     html_content = "<p>#{text}</p><p><a href='https://jsa-agent.herokuapp.com'>Login</a> to your account to reply</p>"
     content = Content.new(type: 'text/html', value: html_content)
     mail = Mail.new(from, subject, to, content)
